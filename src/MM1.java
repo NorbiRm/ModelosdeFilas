f1ca3a3c34655da05ec1bfe05ec4e1df926fe462
Rafael García Cadenas <rafaelgarciacadenas@gmail.com>
2019-04-29 14:19:34 -0500

    MM1
    Versión mejorada del MM1 lista para implementarse
    cualquier duda comunicarse conmigo :v:

--- src/MM1.java
+++ src/MM1.java
@@ -7,16 +7,18 @@ public class MM1 {
     private double wq;
     private double w;
     private double p0;
+    private double pn;
 
-    public MM1(double lambda, double miu, double p, double lq, double l, double wq, double w, double p0) {
+    public MM1(double lambda, double miu, int n) {
         this.lambda = lambda;
         this.miu = miu;
-        this.p = p;
-        this.lq = lq;
-        this.l = l;
-        this.wq = wq;
-        this.w = w;
-        this.p0 = p0;
+        this.p = calculateP();
+        this.p0 = calculateP0();
+        this.pn = calculatePn(n);
+        this.lq = calculateLQ();
+        this.l = calculateL();
+        this.wq = calculateWq();
+        this.w = calculateW();
     }
     public MM1(){
 
@@ -24,19 +26,19 @@ public class MM1 {
 
 
     //Probabilidad sistema desocupado
-    public double calculateP0(double p){
+    public double calculateP0(){
         double p0;
         p0 = 1 - p;
         return p0;
     }
     //factor de utilización
-    public double calculateP(double lambda, double miu){
+    public double calculateP(){
         double p;
         p = lambda/miu;
         return p;
     }
     //número promedio de clientes en la cola
-    public double calculateLQ(double lambda, double miu){
+    public double calculateLQ(){
         double lq;
         double lambda2 = 0;
         lambda2 = Math.pow(lambda, 2);
@@ -44,23 +46,98 @@ public class MM1 {
         return lq;
     }
     //número promedio de clientes en el sistema
-    public double calculateL(double lambda, double miu){
+    public double calculateL(){
         double l;
         l = (lambda/(miu-lambda));
         return l;
     }
     //tiempo esperado en la cola
-    public double calculateWq(double lambda, double miu){
+    public double calculateWq(){
         double wq;
         wq = (lambda/(miu*(miu-lambda)));
         return wq;
     }
     //tiempo promedio en el sistema
-    public double calculateW(double lambda, double miu){
+    public double calculateW(){
         double w;
         w = 1/(miu-lambda);
         return w;
     }
+    public double calculatePn(int n){
+        double pn;
+        pn = (1 - p)*(Math.pow(p,n));
+        return pn;
+    }
 
+    public double getLambda() {
+        return lambda;
+    }
 
+    public void setLambda(double lambda) {
+        this.lambda = lambda;
+    }
+
+    public double getMiu() {
+        return miu;
+    }
+
+    public void setMiu(double miu) {
+        this.miu = miu;
+    }
+
+    public double getP() {
+        return p;
+    }
+
+    public void setP(double p) {
+        this.p = p;
+    }
+
+    public double getLq() {
+        return lq;
+    }
+
+    public void setLq(double lq) {
+        this.lq = lq;
+    }
+
+    public double getL() {
+        return l;
+    }
+
+    public void setL(double l) {
+        this.l = l;
+    }
+
+    public double getWq() {
+        return wq;
+    }
+
+    public void setWq(double wq) {
+        this.wq = wq;
+    }
+
+    public double getW() {
+        return w;
+    }
+
+    public void setW(double w) {
+        this.w = w;
+    }
+
+    public double getP0() {
+        return p0;
+    }
+
+    public void setP0(double p0) {
+        this.p0 = p0;
+    }
+
+    public double getPn() {
+        return pn;
+    }
+
+    public void setPn(double pn) {
+        this.pn = pn;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,19 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        /* Pruebas Comprobación MM1
+        int n = 10;
+        MM1 m = new MM1(1,3,10);
+        System.out.println("P: " + m.getP());
+        System.out.println("P0: " + m.getP0());
+        for(int i = n; i > 0; i-- ){
+            System.out.println("Pn: " + m.calculatePn(i));
+        }
+        System.out.println("Lq: " + m.getLq());
+        System.out.println("L: " + m.getL());
+        System.out.println("Wq: " + m.getWq());
+        System.out.println("w: " + m.getW());
+        */
     }
 
 }
